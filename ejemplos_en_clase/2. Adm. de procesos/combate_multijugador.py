01f4179e0f81cbf1cc4335aa9ed6bb0a0488c2c6
Gunnar Wolf <gwolf@gwolf.org>
2023-09-21 18:36:53 -0600

    Agrego otro parámetro: Número de jugadores

--- ejemplos_en_clase/2. Adm. de procesos/combate_multijugador.py
+++ ejemplos_en_clase/2. Adm. de procesos/combate_multijugador.py
@@ -3,7 +3,8 @@ import threading
 import time
 import random
 
-num_jugadores = 3
+num_jugadores = 5
+total_jugadores = 20
 multiplex = threading.Semaphore(num_jugadores)
 barrera = threading.Semaphore(0)
 activos = []
@@ -39,5 +40,6 @@ def multiplexado(x):
 print('- = { ¡ COMBATE MULTIJUGADOR ! } = -')
 print('¡Bienvenidos concursantes! Ahora van a agarrarse a catorrazos...')
 print('¡De %d en %d!' % (num_jugadores, num_jugadores))
-for i in range(10):
+
+for i in range(num_jugadores):
     threading.Thread(target=multiplexado, args=[i]).start()
