8a199343d83c34cfcb6a5a5c0805ca739d66bcb9
Carlos Garces <carl79garces@gmail.com>
2023-09-24 00:59:29 -0600

    Se agrega la funcion sleep para detener la ejecucion de los hilos y mejorar la visualizacion

--- practicas/4/GarcesCarlos/HolaMundoConcurr.c
+++ practicas/4/GarcesCarlos/HolaMundoConcurr.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 sem_t semaforo1, semaforo2;
 
-void *fun_hilo1() {
+void* fun_hilo1() {
 	while (1){
 		sem_wait(&semaforo1);
 		printf("Hilo 1: ¡Hola!\n" );
+		sleep(2);
 		sem_post(&semaforo2);
 	}
 	return NULL;
 }
 
-void *fun_hilo2() {
+void* fun_hilo2() {
 	while (1){
 		sem_wait(&semaforo2);
 		printf("Hilo 2: ¡Mundo!\n" );
+		sleep(1);
 		sem_post(&semaforo1);
 	}
 	return NULL;
 }
 
 int main() {
-	pthread_t hilo1, hilo2
+	pthread_t hilo1, hilo2;
 
 	sem_init(&semaforo1, 0, 1); //disponible
 	sem_init(&semaforo2, 0, 0); //bloqueado
@@ -34,8 +37,8 @@ int main() {
 
 	pthread_join(hilo1, NULL);
 	pthread_join(hilo2, NULL);
-	sem_destroy(semaforo1);
-	sem_destroy(semaforo2);
+	sem_destroy(&semaforo1);
+	sem_destroy(&semaforo2);
 	
 	return 0;
 }
