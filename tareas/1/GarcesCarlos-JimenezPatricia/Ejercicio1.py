07ce4ae108a00b5d270f22de035c6401500a0440
Carlos Garces <carl79garces@gmail.com>
2023-10-06 23:19:55 -0600

    Planteamiento inicial para la solucion

---
+++ tareas/1/GarcesCarlos-JimenezPatricia/Ejercicio1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# ./Ejercicio1.py
+# ## ###############################################################################################
+# -*- Programa que modela la interaccion jefe-trabajador empleando sincronizacion con semáforos -*-
+# 
+# Autores: Carlos Garcés y Patricia Jimenez
+# ## ###############################################################################################
+
+from threading import Thread, Semaphore
+
+def main():
+	print("Hola mundo!!")
+
+
+if __name__ == '__main__':
+	main()
