f2144bd52197e88bc56c876226d8cc571e733d16
Gunnar Wolf <gwolf@gwolf.org>
2023-10-12 21:35:17 -0300

    No, no quedó resuelto. ¡Me queda de tarea!

--- ejemplos_en_clase/2. Adm. de procesos/banquero.py
+++ ejemplos_en_clase/2. Adm. de procesos/banquero.py
@@ -3,19 +3,25 @@
 # Ilustra una implementación "sencilla" del Algoritmo del Banquero de Djikstra
 # (simplificado a una única categoría de recursos)
 
-procesos = ['A', 'B', 'C', 'D']
+procesos = ['A', 'B', 'C', 'D', 'E', 'F']
 reclamo = {'A': 6,
            'B': 4,
            'C': 6,
-           'D': 3
+           'D': 3,
+           'E': 4,
+           'F': 3
            }
-asignado = {'A': 3, 'B': 0, 'C': 2, 'D': 0}
+asignado = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0}
 # solicita = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
 
-disponibles = 6
+disponibles = 10
 libres = 1
 
 def banquero(quien, cuanto):
+    # Asumiendo que, a partir de la situación actual, yo _otorgara_ la solicitud
+    # de asignar "cuanto" a "quien", ¿es posible llegar a un _estado seguro_ que
+    # permita que todos los procesos terminen, incluso si presentan su reclamo
+    # máximo?
     maximo = reclamo[quien] - asignado[quien]
     print('== Partamos de que: ==')
     print('== Reclamo:  ', reclamo)
@@ -55,5 +61,5 @@ def encuentra_candidato(restantes):
 
 
 ## ↓ Lo siguiente sería el equivalente a un open('recurso') por parte de B
-banquero('B', 1):
+banquero('B', 1)
 print('¡B es feliz!')
