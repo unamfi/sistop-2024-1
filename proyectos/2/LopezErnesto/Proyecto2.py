eeeed90c05a30ee78a9dbbbde3f936054acd44c6
Ernesto Danjiro López Sugahara <danjiro10@gmail.com>
2023-11-02 11:38:19 -0600

    Trabajando la lógica detrás de la interfaz gráfica

--- proyectos/2/LopezErnesto/Proyecto2.py
+++ proyectos/2/LopezErnesto/Proyecto2.py
@@ -1,17 +1,9 @@
-import random
-from threading import Thread, Barrier, Semaphore
+# Import Module 
+from tkinter import *
 from time import sleep
+import random
+from threading import *
 
-"""
-    Se tiene el caso del restaurante Casa de Toño. En este, se tiene un total de 4 mesas para 4 personas,
-    4 mesas para 2 personas y 4 mesas para 1 persona. Conforme van llegando los clientes, se tiene que validar que haya una mesa
-    disponible para la cantidad de personas que forman el grupo. El problema siguiente se encarga de gestionar la fila de Casa
-    de Toño.
-
-    En este caso se quiere evitar que el restaurante sobrepase el límite de personas que pueden estar dentro. Igualmente, se requiere que
-    únicamente se vaya avisando a cada una de las familias con el respectivo turno (siempre que se libere una mesa para esa cantidad) que
-    puede acceder al restaurante.
-"""
 # Número de familias esperadas
 num_familias = 50
 
@@ -36,7 +28,14 @@ colaCuatro = {}
 # Comiendo
 comiendo = {}
 
-def llegaFamilia(fam,tam):
+def threading(): 
+    # Call work function 
+    for i in range(10):
+        # La familia tendrá un tamaño aleatorio de 4, 2 o 1
+        Thread(target=work,args=[i,tamaños[random.randint(0,2)]]).start()
+  
+# work function 
+def work(fam,tam): 
     mutexLlegada.acquire() # Se entra a una zona crítica
     print(comiendo)
     print(f"La familia {fam} de tamaño {tam} llegó al establecimiento")
@@ -45,11 +44,12 @@ def llegaFamilia(fam,tam):
         # Se debe de verificar que haya mesa para una persona
         mutexLlegada.release()
         colaUno[fam] = tam
+        print(colaUno)
         multiplexUno.acquire()
         comiendo[fam] = tam
         del colaUno[fam]
         # Tendrá un tiempo específico
-        sleep(random.random() * 20)
+        sleep(random.random() * 20) # Modificar el tiempo
         print(f"La familia {fam} de tamaño {tam} terminó de comer")
         # En este caso, se desocupa el lugar y se van 
         del comiendo[fam]
@@ -82,10 +82,50 @@ def llegaFamilia(fam,tam):
         multiplexCuatro.release()
         pass
     pass
+    
+# Create Object 
+root = Tk() 
+  
+# Definiendo la interfaz gráfica
 
+root.title("Casa de Toño")
+# Crear un frame para el borde del restaurante
+frame_restaurante = Frame(root,bg="dark green")
+frame_restaurante.grid(row=0, column=0, rowspan=4, columnspan=3, sticky="nsew",padx=30,pady=30)
 
-# Consideraremos un flujo de 50 familias que irán llegando al restaurante:
-for i in range(50):
-    sleep(random.random())
-    # La familia tendrá un tamaño aleatorio de 4, 2 o 1
-    Thread(target=llegaFamilia,args=[i,tamaños[random.randint(0,2)]]).start()
+frame_mesas = Frame(frame_restaurante,bg="white")
+frame_mesas.grid(row=0, column=0, rowspan=4, columnspan=3, sticky="nsew",padx=10,pady=10)
+
+# Configuración de las filas y columnas del frame_mesas
+for i in range(1,5):
+    frame_mesas.grid_rowconfigure(i, weight=1)
+for i in range(3):
+    frame_mesas.grid_columnconfigure(i, weight=1)
+
+# Crear botones para representar las mesas dentro del frame
+mesas = []
+for fila in range(4):
+    for columna in range(3):
+        mesa_numero = fila * 3 + columna + 1
+        mesa_button = Button(
+            frame_mesas,
+            text=f"Mesa {mesa_numero}",
+            width=20,
+            height=6,
+        )
+        mesa_button.grid(row=fila, column=columna, padx=40, pady=30)
+        mesas.append(mesa_button)
+
+# Agregamos un botón más para comenzar a realizar el procedimiento:
+abrir = Button(
+            frame_mesas,
+            text=f"Abrir restaurante",
+            width=10,
+            height=3,
+            # command=lambda: abrirRestaurante(self),
+            command=threading
+        )
+abrir.grid(row=6,column=1)
+  
+# Execute Tkinter 
+root.mainloop()
