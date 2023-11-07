b0af7856d85ec388dc9b77634ad19c28b942ad68
Sebastian Vazquez <vazquezsebas946@gmail.com>
2023-11-06 20:38:14 -0600

    Proyecto 2 actualizado. Funciones agregadas

--- proyectos/2/VazquezSebastian/proyecto02.py
+++ proyectos/2/VazquezSebastian/proyecto02.py
@@ -6,20 +6,44 @@ import threading
 import time
 import random
 
-accesoCompra = threading.Semaphore(10)
-accesoPago1 = threading.Lock()
-accesoPago2 = threading.Lock()
-accesoPago3 = threading.Lock()
+accesoCompra = threading.Semaphore(20)
+accesoPago = threading.Lock()
 
-conciertosDisponibles= [['15','BadBunny'],['20','Coldplay'], ['10','Imagine Dragons']]
+conciertosDisponibles= [[10,'BadBunny', 500],[8,'Coldplay', 350], [8,'Imagine Dragons', 250]]
 
 def comprador(id): 
-    global accesoCompra, accesoPago1, accesoPago2, accesoPago3
+    tarjeta = random.randint(10, 20) * 50   
+    conciertoelegido = random.randint(0, 2) 
+    print ("Soy el comprador numero "+ id + " y cuento con " + tarjeta +" pesos en mi tarjeta, y quiero ver a "+ conciertosDisponibles[conciertoelegido][1])
+    dormido = random.randint(1, 9) * 0.015
+    time.sleep(dormido) 
+    compraBoleto(id,tarjeta, conciertoelegido)
 
+def compraBoleto(id, tarjeta, con):
+    global accesoCompra, accesoPago
+    accesoCompra.acquire
+    boletos = random.randint(1, 3)
+    print ("Soy el comprador numero "+ id + " y quiero comprar " + boletos +" boleto(s)")
+    precio = conciertosDisponibles[con][2] * boletos
+    if(tarjeta>=precio):
+        if (conciertosDisponibles[con][0]>=boletos):
+            pagarBoleto(tarjeta, precio, con, boletos)
+            print ("Soy el comprador numero "+ id + " y tengo " + boletos +" boletos para ver a "+ conciertosDisponibles[con][1])
+        else:
+            print ("Ya no quedan tantos boletos, no puedo comprarlos")
+    else:
+        print ("No puedo pagar el/los boleto(s), salgo de la cola")
+    accesoCompra.release
+
+def pagarBoleto(tarjeta, precio, con, boletos):
+    global accesoPago
+    accesoPago.acquire
+    tarjeta -= precio
+    conciertosDisponibles[con][0] -= boletos
+    accesoPago.release
 	
 def main():
     numCompradores = int(input("¿Cuantos compradores en total hay hoy?: (de preferencia un numero mayor a 20)"))
-
     compradores = [threading.Thread(target = comprador, args=[i]).start() for i in range (numCompradores)]
 
 main()
