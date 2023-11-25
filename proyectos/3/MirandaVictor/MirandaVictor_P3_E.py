076318c8cebbffab86417dc719f7a53b85fbd194
VictorMB-27 <victor.miranda.barajas@gmail.com>
2023-11-25 03:30:44 -0600

    Se agregó la función  copiar a fiunamfs en el archivo .py

--- proyectos/3/MirandaVictor/MirandaVictor_P3_E.py
+++ proyectos/3/MirandaVictor/MirandaVictor_P3_E.py
@@ -53,3 +53,39 @@ def copiar_a_sistema(fiunamfs_img, nombre_archivo, destino):
                     return
     raise FileNotFoundError("Archivo no encontrado en FiUnamFS")
 
+def copiar_a_fiunamfs(fiunamfs_img, archivo_origen, nombre_destino):
+    with open(fiunamfs_img, 'r+b') as f:
+        tam_origen = os.path.getsize(archivo_origen)
+        cluster_libre = 5
+        posicion_entrada_libre = None
+
+        f.seek(DIRECTORIO_INICIO)
+        for _ in range(DIRECTORIO_TAMANO // TAMANO_ENTRADA):
+            posicion_actual = f.tell()
+            entrada = f.read(TAMANO_ENTRADA)
+            tipo_archivo = entrada[0:1]
+            cluster_ini = struct.unpack('<I', entrada[20:24])[0]
+
+            if tipo_archivo == b'/' and posicion_entrada_libre is None:  # Verifica si la entrada está vacía
+                posicion_entrada_libre = posicion_actual
+                print(f"Encontrada entrada libre en posición {posicion_entrada_libre}")  # Para depuración
+
+            if cluster_ini >= cluster_libre:
+                cluster_libre = cluster_ini + 1
+                print(f"Nuevo cluster libre: {cluster_libre}")  # Para depuración
+
+        if posicion_entrada_libre is None:
+            raise Exception("No hay espacio en el directorio")
+        else:
+            print(f"Espacio libre en la entrada: {posicion_entrada_libre}, Cluster libre para el archivo: {cluster_libre}")  # Para depuración
+
+        with open(archivo_origen, 'rb') as archivo_origen_f:
+            f.seek(cluster_libre * TAMANO_CLUSTER)
+            f.write(archivo_origen_f.read())
+
+        f.seek(posicion_entrada_libre)
+        f.write(b'-' + nombre_destino.ljust(15).encode('ascii'))
+        f.write(struct.pack('<I', tam_origen))
+        f.write(struct.pack('<I', cluster_libre))
+
+
