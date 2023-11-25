d4ba434b8cfeb2ec51c8a426e7e8f962bf551573
VictorMB-27 <victor.miranda.barajas@gmail.com>
2023-11-25 03:24:44 -0600

    Se agrega el archivo .py con la función de Superbloque

---
+++ proyectos/3/MirandaVictor/MirandaVictor_P3_E.py
@@ -0,0 +1,24 @@
+#Miranda Barajas Victor
+import os
+import struct
+
+
+TAMANO_CLUSTER = 1024
+TAMANO_ENTRADA = 64
+DIRECTORIO_INICIO = TAMANO_CLUSTER  # Cluster 1
+DIRECTORIO_TAMANO = 4 * TAMANO_CLUSTER  # 4 clusters para el directorio
+MAXIMO_CLUSTERS = 1440 // 4  # Asumiendo que el tamaño total es 1440KB
+
+def leer_superbloque(fiunamfs_img):
+    with open(fiunamfs_img, 'rb') as f:
+        f.seek(0)
+        nombre_fs = f.read(8).decode('ascii').strip()
+        f.seek(10)
+        version = f.read(5).decode('ascii').rstrip('\x00').strip()
+        print(f"Nombre FS leído: {nombre_fs}, Versión leída: {version}")
+        #print(f"Hexadecimal: {nombre_fs.encode('ascii').hex()}, {version.encode('ascii').hex()}")
+        if nombre_fs != "FiUnamFS" or version.strip() != "24.1":
+            print("Valores no coinciden, se esperaba FiUnamFS y  24.1")
+            #raise ValueError("No es un sistema FiUnamFS válido o versión no soportada.")
+        else:
+            print("Superbloque válido") 
